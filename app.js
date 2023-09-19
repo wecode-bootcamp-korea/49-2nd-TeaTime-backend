34331d24ad6a65f1b9fcfe7ea356e0cf87ad28c0
imjeongjin <kjj_010101@naver.com>
2023-09-19 19:59:48 +0900

    초기환경설정

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+const {myDataSource} = require("./src/models/dataSource");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/ping", async(req, res) =>{
+    try {
+        return res.status(200).json({
+            message : "pong",
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+const start = async () => {
+    try {
+        await myDataSource.initialize().then(() => console.log("Data Source has been initialized!"));
+
+        const port = process.env.SERVER_PORT;
+        app.listen(port, () => console.log(`server is listening in PORT ${port}`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
