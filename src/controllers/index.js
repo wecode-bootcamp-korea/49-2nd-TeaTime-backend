9f26304cf8fb2ca314b82a30713a22dd8fa568eb
49기 이재훈 <handymansodo@gmail.com>
2023-09-25 14:35:17 +0900

    idx수정

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const productController = require("./productController");
 const cartController = require("./cartController")
 module.exports = {
-  productController,cartController
+  productController,
+  cartController
 };
--- src/models/index.js
+++ src/models/index.js
@@ -7,4 +7,5 @@ module.exports = {
   userDao,
   productDao,
   productQueryBuilder,
+  cartDao
 };
