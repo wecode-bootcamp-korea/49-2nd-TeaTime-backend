35ed5a59245299f4309f57a03d0d8de4cb74187a
49기 이재훈 <handymansodo@gmail.com>
2023-09-27 00:03:24 +0900

    router 수정

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { productRouter } = require("./productRouter");
+const { cartRouter } = require("./cartRouter")
 const router = express.Router();
 
 router.use("/products", productRouter);
-
+router.use("/cart", cartRouter)
 module.exports = router;
